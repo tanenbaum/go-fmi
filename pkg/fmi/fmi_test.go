5668e0ed2b9b5d28bc6b38ffb37c7043be05db70
Joe Shaw <joe.r.shaw@gmail.com>
2021-04-19 15:04:19 +0100

    GetFMUState test

--- pkg/fmi/fmi_test.go
+++ pkg/fmi/fmi_test.go
@@ -139,6 +139,17 @@ func (m mockInstance) DoStep(
 	return m.stepResult, nil
 }
 
+func (m mockInstance) Encode() ([]byte, error) {
+	if m.err {
+		return nil, errors.New("Encode")
+	}
+	return []byte("foo"), nil
+}
+
+func (m mockInstance) Decode(_ []byte) error {
+	return m.errOrNil("Decode")
+}
+
 func noopLogger(status fmi.Status, category, message string) {}
 
 // model setup for testing
@@ -1293,6 +1304,56 @@ func TestDoStep(t *testing.T) {
 			if got := fmi.DoStep(tt.args.id, tt.args.currentCommunicationPoint, tt.args.communicationStepSize, tt.args.noSetFMUStatePriorToCurrentPoint); got != tt.want {
 				t.Errorf("DoStep() = %v, want %v", got, tt.want)
 			}
+			fmi.FreeInstance(fmi.FMUID(tt.args.id))
+		})
+	}
+}
+
+func TestGetFMUState(t *testing.T) {
+	type args struct {
+		id fmi.FMUID
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  []byte
+		want1 fmi.Status
+	}{
+		{
+			"Model state is invalid",
+			args{
+				instantiateDefault(fmi.ModelStateStartAndEnd),
+			},
+			nil,
+			fmi.StatusError,
+		},
+		{
+			"State encoder error is handled",
+			args{
+				instantiateInstanceErrors(),
+			},
+			nil,
+			fmi.StatusError,
+		},
+		{
+			"State is encoded to bytes",
+			args{
+				instantiateDefault(),
+			},
+			[]byte("foo"),
+			fmi.StatusOK,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got1 := fmi.GetFMUState(tt.args.id)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetFMUState() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("GetFMUState() got1 = %v, want %v", got1, tt.want1)
+			}
+			fmi.FreeInstance(fmi.FMUID(tt.args.id))
 		})
 	}
 }
